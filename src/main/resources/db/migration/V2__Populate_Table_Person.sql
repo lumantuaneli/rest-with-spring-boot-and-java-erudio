29771bc7c0f7715b546b48bca194644b51da5155
lumantuaneli <lumantuaneli@gmail.com>
2024-05-29 10:11:04 -0300

    Aula 1103 - Migrations com Flyway

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, '3175 Raposo Tavares Road, Ap 66F - São Paulo - São Paulo', 'Luciano', 'Male', 'Mantuaneli'),
+	(2, '3175 Raposo Tavares Road, Apt 66-F - São Paulo city - São Paulo', 'Noemia', 'Female', 'Alcarde Mantuaneli'),
+	(5, '754 Waldemar Meira St - Cajamar - São Paulo', 'Clarice', 'Female', 'Caldeira Mantuaneli'),
+	(7, '161 Águas Formosas - São Paulo - São Paulo', 'Alexandre', 'Male', 'Mantuaneli'),
+	(8, '161 Águas Formosas - São Paulo city - São Paulo', 'Elisângela', 'Male', 'Cruz Mantuaneli');
